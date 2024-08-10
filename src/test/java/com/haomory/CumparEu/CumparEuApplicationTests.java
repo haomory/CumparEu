1eb2d6e6b008797adeef64e5622e185e24e11a32
haomory <haomory@gmail.com>
2024-08-10 13:12:01 +0300

    initial

---
+++ src/test/java/com/haomory/CumparEu/CumparEuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.haomory.CumparEu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CumparEuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
